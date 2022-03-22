4a5421694bc2aea2abcc23aec7b211f8cdb10cd6
hey-kong <iggiewang@gmail.com>
2022-03-22 10:37:50 +0800

    add some packages

---
+++ pkg/common/constants.go
@@ -0,0 +1,25 @@
+/*
+Copyright 2021 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package common
+
+const (
+	// ServerRootPath is the root path of server
+	ServerRootPath = "metaedge"
+
+	// DataBaseURL is url of database
+	DataBaseURL = "/var/lib/metaedge/database.db"
+)
